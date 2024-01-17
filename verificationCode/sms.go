ace3044bd4e399d19f3d6274b695c5718cea48b8
Dan Chen <dan.chen@boardware.com>
2024-01-17 10:31:01 +0800

    feat

--- verificationCode/sms.go
+++ verificationCode/sms.go
@@ -4,36 +4,37 @@ import (
 	"hotelfortuna/common/config"
 
 	"github.com/Dparty/common/sms"
-	"github.com/universalmacro/common/sms/models"
-	tencentSms "github.com/universalmacro/common/sms/tencent"
 )
 
-// const chainTemplateId = "914223"
-// const internationTemplateId = "914222"
+const chainTemplateId = "914223"
+const internationTemplateId = "914222"
 
-// var sendCloud *sms.SendCloud = sms.NewSendCloud(config.GetString("sendCloud.user"), config.GetString("sendCloud.key"))
-var tencentSmsSender = tencentSms.NewSmsSender(config.GetString("tencentCloud.secretId"), config.GetString("tencentCloud.secretKey"))
+var sendCloud *sms.SendCloud = sms.NewSendCloud(config.GetString("sendCloud.user"), config.GetString("sendCloud.key"))
 
 func SendVerificationCode(to sms.PhoneNumber, code string) {
-	config := tencentSms.Config{
-		AppId:      "2400001857",
-		SignName:   "財神酒店",
-		TemplateId: "2910166",
-	}
-	vars := []string{code}
-	err := tencentSmsSender.SendWithConfig(models.PhoneNumber{AreaCode: to.AreaCode, Number: to.Number}, config, vars)
-	if err != nil {
-		println(err)
+	sendCloud.SendWithTemplate(to, getTemplateIdByAreaCode(to.AreaCode), map[string]string{"code": code})
+}
+
+func getTemplateIdByAreaCode(areaCode string) string {
+	switch areaCode {
+	case "86":
+		return chainTemplateId
+	case "853", "852":
+		return internationTemplateId
+	default:
+		return ""
 	}
 }
 
-// func getTemplateIdByAreaCode(areaCode string) string {
-// 	switch areaCode {
-// 	case "86":
-// 		return chainTemplateId
-// 	case "853", "852":
-// 		return internationTemplateId
-// 	default:
-// 		return ""
+// func SendVerificationCode(to sms.PhoneNumber, code string) {
+// 	config := tencentSms.Config{
+// 		AppId:      "2400001857",
+// 		SignName:   "財神酒店",
+// 		TemplateId: "2910166",
+// 	}
+// 	vars := []string{code}
+// 	err := tencentSmsSender.SendWithConfig(models.PhoneNumber{AreaCode: to.AreaCode, Number: to.Number}, config, vars)
+// 	if err != nil {
+// 		println(err)
 // 	}
 // }
